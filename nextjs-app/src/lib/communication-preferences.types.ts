dd2170fa462af848ff2f8cbfbc01180e4cc3f715
smokefreeksa-ops <smokefreeksa@gmail.com>
2026-08-20 18:33:08 +0100

    Separate communication preference types from AWS server code

---
+++ nextjs-app/src/lib/communication-preferences.types.ts
@@ -0,0 +1,3 @@
+export type EmailCategory = 'transactional' | 'followup' | 'research'
+export type UnsubscribeScope = 'followup' | 'research' | 'all_non_transactional'
+export type SuppressionReason = 'hard_bounce' | 'complaint' | 'manual' | 'invalid_address'
